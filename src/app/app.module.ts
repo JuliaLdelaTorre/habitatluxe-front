378b3c438aa96531146ff1f6e5faa848777c4977
DESKTOP-9SLAVMB\Julia <lopezdltjulia@gmail.com>
2024-03-19 19:41:28 +0100

    Sass, Bem. Nosotros page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NosotrosComponent,
+
   ],
   imports: [
     BrowserModule,
