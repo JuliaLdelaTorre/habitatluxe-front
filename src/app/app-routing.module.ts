fb4e1557b641b67012e5f9df25d2b9db3e06fd7d
DESKTOP-9SLAVMB\Julia <lopezdltjulia@gmail.com>
2024-03-25 16:58:21 +0100

    add routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // inicio de la aplicación (landing).
+  { path: '', component: HomeComponent },
+  // ruta a la página "Nosotros."
+  { path: 'nosotros', component: NosotrosComponent },
+
+  // Con carga perezosa, no se carga el módulo hasta que se navega a la ruta.
+  { path: 'anuncios', loadChildren: () => import('./anuncios/anuncios.module').then(m => m.AnunciosModule) },
+  { path: 'contacto', loadChildren: () => import('./contacto/contacto.module').then(m => m.ContactoModule)},
+  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)},
+
+  // cualquier otra ruta que no esté arriba, redirigir a home.
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
