5d4c2dabba852800f85eba91315d5545dd1526f6
Julia Lopez <lopezdltjulia@gmail.com>
2024-04-10 13:57:51 +0200

    properties component created

--- src/app/properties/properties.module.ts
+++ src/app/properties/properties.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PropertiesComponent } from './properties.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PropertiesComponent
+  ],
   imports: [
     CommonModule
   ]
