b82730eafc0686309b99be28157f7ff2c3b28156
Julia Lopez <lopezdltjulia@gmail.com>
2024-05-13 08:31:43 +0200

    actualizacion código

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/app/environments/environments';
+import { LoginResponse } from '../interfaces/loginResponse.interface';
 
 @Injectable({providedIn: 'root'})
 
 export class AuthService {
-
-  // private readonly baseUrl: string = environment.baseUrl;
-
-  constructor() { }
 
 }
