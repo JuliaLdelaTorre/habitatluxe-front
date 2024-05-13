19cf83047cec576797b738c3ff8bbe05fb3ee43e
Julia Lopez <lopezdltjulia@gmail.com>
2024-05-13 13:05:14 +0200

    mainLayout upated with header & footer

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   <shared-header></shared-header>
   <router-outlet></router-outlet>
   <shared-footer></shared-footer>
-  `
+  `,
+  styles: [`
+  :host {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  router-outlet {
+    flex-grow: 1;
+  }
+  `]
 })
 export class MainLayoutComponent {
 
