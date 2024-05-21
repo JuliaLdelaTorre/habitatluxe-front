0771e4aca9b6180ab0a5bc2dc35251d2ad9dd027
Julia Lopez <lopezdltjulia@gmail.com>
2024-05-21 17:30:09 +0200

    blog response updated

--- src/app/blog/pages/blog-page/blog-page.component.ts
+++ src/app/blog/pages/blog-page/blog-page.component.ts
@@ -13,18 +13,13 @@ export class BlogPageComponent implements OnInit{
 
   constructor( private blogService: BlogService ) { }
 
-  // array - con json-sever
-  ngOnInit(): void {
-    this.blogService.getBlog()
-    .subscribe( blog => this.blog = blog)
-  }
 
 //objeto - con backend laravel.
-  // ngOnInit(): void {
-  //   this.blogService.getBlog()
-  //   .subscribe((response: any) => {
-  //     this.blog = response.data;
-  //   });
-  // }
+  ngOnInit(): void {
+    this.blogService.getBlog()
+    .subscribe((response: any) => {
+      this.blog = response.data;
+    });
+  }
 
 }
