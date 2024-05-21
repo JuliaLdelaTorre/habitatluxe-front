ab704858b75c574e7e41f0228a86efbc2db7e498
Julia Lopez <lopezdltjulia@gmail.com>
2024-05-21 17:22:43 +0200

    regular expressions add as validators

---
+++ src/app/shared/validators/validators.ts
@@ -0,0 +1,6 @@
+
+
+export const namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+export const passwordPatern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})";
+
