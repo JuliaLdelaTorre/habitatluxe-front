eea4d0a731fb9061e954f7e3096850af04d1c768
Julia Lopez <lopezdltjulia@gmail.com>
2024-05-22 13:39:40 +0200

    validators updated

--- src/app/shared/validators/validators.ts
+++ src/app/shared/validators/validators.ts
@@ -1,6 +1,8 @@
 
-
-export const namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
-export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
-export const passwordPatern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})";
+// Debe ser nombre y apellido.
+export const name = '([a-zA-Z]+) ([a-zA-Z]+)';
+// Debe tener el formato de un correo electronico.
+export const email: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+// Debe contener minusculas, mayusculas, numeros, caracteres especiales y tener una longitud de 6 caracteres.
+export const password = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})";
 
