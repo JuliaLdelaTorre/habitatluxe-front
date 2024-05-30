e958d7d6fe72b5abb8cba5f320d8855c26786a60
Julia Lopez <lopezdltjulia@gmail.com>
2024-05-30 11:19:02 +0200

    .

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as validators from '../../../shared/validators/validators';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { email, password } from '../../../shared/validators/validators';
 
 @Component({
   selector: 'app-login-page',
@@ -13,20 +17,33 @@ export class LoginPageComponent implements OnInit {
 
   loginForm: FormGroup = this.formBuilder.group({});
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+
+      email: ['', [Validators.required, Validators.pattern(validators.email)]],
+      password: ['', [Validators.required, Validators.pattern(validators.password)]]
     });
   }
 
   onSubmitLogin() {
-    if (this.loginForm) {
-      console.log('Formulario válido');
-
-    }
-
+    // if (this.loginForm && this.loginForm.valid) {
+    //   this.authService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe(
+    //     (response) => {
+    //       this.router.navigate(['/home']);
+    //     },
+    //     (error) => {
+    //       console.log('Error al iniciar sesión', error);
+    //     }
+    //   );
+    //   }
   }
+
+
+
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -8,4 +8,31 @@ import { LoginResponse } from '../interfaces/loginResponse.interface';
 
 export class AuthService {
 
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject(HttpClient);
+
+  // propiedades privadas para almacenar el estado de la autenticación.
+  // comienzan con _  sólo para indicar que son privadas.
+
+  // private _currentUser = signal<User | null>(null);
+  // private _authStatus = signal<AuthStatus>();
+
+
+  constructor() { }
+
+  // login(email: string, password: string): Observable<boolean> {
+
+  //   const url = `${this.baseUrl}/auth/login`;
+  //   const body = { email, password };
+
+
+  //    return this.http.post<LoginResponse>(url, body)
+  //    .pipe(
+  //     tap( ({ user, token })  => {
+  //       this._
+  //     })
+  //    )
+
+
+  // }
 }
