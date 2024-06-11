4082e07ae2d4f3fb09045170a740759b43a11127
DESKTOP-9SLAVMB\Julia <lopezdltjulia@gmail.com>
2024-06-11 11:00:32 +0200

    Auto-generated commit

---
+++ es/102.4096b4a3f89f9300.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhabitatluxe_front=self.webpackChunkhabitatluxe_front||[]).push([[102],{3102:(h,s,t)=>{t.r(s),t.d(s,{BlogModule:()=>u});var l=t(6814),n=t(6208),o=t(9468);let u=(()=>{class a{static#t=this.\u0275fac=function(_){return new(_||a)};static#a=this.\u0275mod=o.oAB({type:a});static#s=this.\u0275inj=o.cJS({imports:[l.ez,n.m]})}return a})()}}]);
