fe8df95b8913849220dc3423f94e90fe0cbdf206
DESKTOP-9SLAVMB\Julia <lopezdltjulia@gmail.com>
2024-06-11 10:11:56 +0200

    footer updated

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ThemeService } from 'src/app/pages/home/theme.service';
+
 
 @Component({
   selector: 'shared-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  lightMode: boolean = true;
+
+  constructor( private themeService: ThemeService) { }
+
+
+
+
 
 }
